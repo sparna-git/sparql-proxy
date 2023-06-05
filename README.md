bdf918396df8a63ff84978828da041d257625970
Thomas Francart <thomas.francart@sparna.fr>
2023-06-05 17:20:07 +0200

    Update index.html

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# sparql-proxy
-SPARQL Proxy to avoid CORS issue when queried from Sparnatural clients
+# SPARQL Proxy
+
+A SPARQL proxy server, deployed at https://proxy.sparnatural.eu.
+This proxy avoids 2 security issues when querying a SPARQL service from a webpage (e.g. from [Sparnatural](https://sparnatural.eu)):
+1. SPARQL servers that are not CORS-enabled
+2. SPARQL services that do no run on https, while the client does
--- src/main/java/fr/sparna/rdf/sparnatural/proxy/SparqlProxyController.java
+++ src/main/java/fr/sparna/rdf/sparnatural/proxy/SparqlProxyController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.rdf4j.http.client.SPARQLProtocolSession;
 import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.eclipse.rdf4j.query.BooleanQuery;
 import org.eclipse.rdf4j.query.GraphQuery;
@@ -24,18 +25,25 @@ import org.eclipse.rdf4j.repository.sparql.SPARQLRepository;
 import org.eclipse.rdf4j.rio.RDFWriter;
 import org.eclipse.rdf4j.rio.rdfxml.RDFXMLWriter;
 import org.eclipse.rdf4j.rio.turtle.TurtleWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SparqlProxyController {
 
+	private Logger log = LoggerFactory.getLogger(this.getClass().getName());
+	
 	
 	@RequestMapping(
-			value = {"sparql"},
-			params={"query", "endpoint"}
+			value = {"sparql"},			
+			params={"endpoint"}
+			// params={"query", "endpoint"}
 	)
 	public void sparql(
 			@RequestParam(value="query", required=true) String query,
@@ -46,6 +54,18 @@ public class SparqlProxyController {
 			HttpServletRequest request,
 			HttpServletResponse response
 	) throws IOException {
+		
+		log.debug("sparql-proxy/sparql (endpoint="+endpoint+" method="+method+" format="+format+" accept="+accept+")");
+		log.debug(query);
+		
+		// fix the query for "bif:contains" so that parsing of Virtuoso queries succeeds
+		if(
+				query.contains("bif:")
+				&&
+				!query.contains("PREFIX bif:")
+		) {
+			query = "PREFIX bif: <bif:>"+"\n"+query;
+		}
 		
 		SPARQLRepository sparqlRepository = new SPARQLRepository(endpoint);
 		RepositoryConnection connection = sparqlRepository.getConnection();
@@ -65,10 +85,12 @@ public class SparqlProxyController {
 				case "json" : {
 					response.setContentType("application/sparql-results+json");
 					handler = new SPARQLResultsJSONWriter(response.getOutputStream());
+					break;
 				}
 				default : {
 					response.setContentType("application/sparql-results+xml");
 					handler = new SPARQLResultsXMLWriter(response.getOutputStream());
+					break;
 				}
 				}
 			} else {
@@ -77,23 +99,28 @@ public class SparqlProxyController {
 				case "application/sparql-results+json" : {
 					response.setContentType("application/sparql-results+json");
 					handler = new SPARQLResultsJSONWriter(response.getOutputStream());
+					break;
 				}
 				case "text/csv" : {
 					response.setContentType("text/csv");
 					handler = new SPARQLResultsCSVWriter(response.getOutputStream());
+					break;
 				}
 				case "text/tab-separated-values" : {
 					response.setContentType("text/tab-separated-values");
 					handler = new SPARQLResultsTSVWriter(response.getOutputStream());
+					break;
 				}
 				default : {
 					response.setContentType("application/sparql-results+xml");
 					handler = new SPARQLResultsXMLWriter(response.getOutputStream());
+					break;
 				}
 				}	
 			}
 		
-			
+			// set http method right before execution
+			setMethod(method);
 			q.evaluate(handler);
 		} else if(pq instanceof ParsedGraphQuery) {
 			GraphQuery q = connection.prepareGraphQuery(query);
@@ -103,19 +130,48 @@ public class SparqlProxyController {
 			case "text/turtle" : {
 				response.setContentType("text/turtle");
 				handler = new TurtleWriter(response.getOutputStream());
+				break;
 			}
 			default : {
 				response.setContentType("application/rdf+xml");
 				handler = new RDFXMLWriter(response.getOutputStream());
+				break;
 			}
 			}	
 			
+			// set http method right before execution
+			setMethod(method);
 			q.evaluate(handler);
 			
 		} else if(pq instanceof ParsedBooleanQuery) {
 			BooleanQuery q = connection.prepareBooleanQuery(query);
+			// set http method right before execution
+			setMethod(method);
 			boolean b = q.evaluate();
 			response.getOutputStream().write(Boolean.toString(b).getBytes());
+		}
+	}
+	
+	private void setMethod(String method) {
+		// set the method
+		if(method != null) {
+			switch(method.toUpperCase()) {
+			case "GET" : {
+				System.setProperty("rdf4j.sparql.url.maxlength", Integer.toString(Integer.MAX_VALUE));
+				break;
+			}
+			case "POST" : {
+				System.setProperty("rdf4j.sparql.url.maxlength", "1");
+				break;
+			}
+			default : {
+				// unknown method set
+				System.setProperty("rdf4j.sparql.url.maxlength", Integer.toString(SPARQLProtocolSession.DEFAULT_MAXIMUM_URL_LENGTH));
+				break;
+			}
+			}
+		} else {
+			System.setProperty("rdf4j.sparql.url.maxlength", Integer.toString(SPARQLProtocolSession.DEFAULT_MAXIMUM_URL_LENGTH));
 		}
 	}
 	
